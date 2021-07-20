b9dd9c00558e2f355fca78a32166703ed9e8123d
namrata giduturi <71811931+namratanimmy@users.noreply.github.com>
2021-07-20 15:14:52 +0530

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+	feature1 = [float(x) for x in request.form.values('.closeprice')]
+	feature2 = [float(x) for x in request.form.values('.compound')]
+	feature1 = np.array(feature1)
+	feature2 = np.array(feature2)
+
+	final_features = pd.concat([feature1, feature2], axis=1)
+	prediction = model.predict(final_features)
+
+
+    # float_features = [float(x) for x in request.form.values()]
+    # final_features = [np.array(float_features)]
+    # prediction = model.predict(final_features)
+
+    # output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Close Price for next day is ${}'.format(prediction))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
